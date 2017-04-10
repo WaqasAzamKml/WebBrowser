f32579a5c0e47fb88715e9cd5d9a7f5aa40d9b00
Muhammad Waqas <waqasazamkml@gmail.com>
2017-04-10 17:47:46 +0500

    Added WebView and its basic functionality.

--- app/src/main/java/com/dozydroid/webbrowser/MainActivity.java
+++ app/src/main/java/com/dozydroid/webbrowser/MainActivity.java
@@ -2,12 +2,43 @@ package com.dozydroid.webbrowser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
+    Button button;
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.btnVisit);
+        webView = (WebView) findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!TextUtils.isEmpty(editText.getText())){
+                    String url = editText.getText().toString();
+                    webView.loadUrl("http://"+url);
+                    editText.clearFocus();
+                }else{
+                    Toast.makeText(MainActivity.this, "Please enter a URL", Toast.LENGTH_SHORT).show();
+                    editText.setError("URL Required");
+                    editText.requestFocus();
+                }
+            }
+        });
     }
 }
